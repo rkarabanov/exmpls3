af81af437852ae21439b38d4a8d7ea724b85cec8
rkarabanov <5600t0@gmail.com>
2016-05-29 17:59:52 +0300

    add router

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-/**
- * Created by rkara on 29.05.2016.
- */
+'use strict';
+phonecatApp
+    .config(['$routeProvider','$locationProvider', function ($routeProvide,$locationProvide) {
+    $routeProvide
+        .when("/",
+            {
+                templateUrl: 'template/home.html',
+                controller: 'PhoneListCtrl'
+            }
+        )
+        .when('/phones/:phoneId', {
+            templateUrl: 'template/mobile_details.html',
+            controller: 'PhoneDetailsCtrl'
+        });
+}]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,13 @@
 'use strict';
-var phonecatApp=angular.module('phonecatApp',[]);
+var phonecatApp=angular.module('phonecatApp',['ngRoute']);
 
-phonecatApp.controller('PhoneListCtrl',['$scope','$http',function ($scope, $http) {
-    $http.get('phones/phones.json').success(function (data,status,headers,config) {
+phonecatApp.controller('PhoneListCtrl',['$scope','$http', '$location',function ($scope, $http, $location) {
+    $http.get('phones/phones.json').success(function (data) {
         $scope.phones=data;
     });
     
+}]);
+
+phonecatApp.controller('PhoneDetailsCtrl',['$scope','$http', '$location', '$routeParams',function ($scope, $http, $location,$routeParams) {
+  $scope.phoneId=$routeParams.phoneId;
 }]);
