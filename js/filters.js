390001fc7718e901ca4be2cbeca5c84e25185895
rkarabanov <5600t0@gmail.com>
2016-05-31 14:34:10 +0300

    add camelcase filter

--- js/filters.js
+++ js/filters.js
@@ -3,3 +3,14 @@ phonecatApp.filter('checkmark',function () {
         return input? '+':'-';
     }
 })
+     .filter('samelcase',function () {
+         return function (input) {
+             strings=input.split(' ');
+             for (let i=0;i<strings.length;i++){
+                 let str=strings[i].split('');
+                 str[0]= str[0].toLowerCase();
+                 strings[i]=str.join('')
+             }
+             return strings.join(' ');
+         }
+     });
